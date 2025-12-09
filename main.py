d9bac14f414ae8e3ffea93fa5699401c4ebea0a8
oriolcorbella <154092801+oriolcorbella@users.noreply.github.com>
2025-12-09 19:05:42 +0100

    Actualització pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+x = 2
+y = 2
 
+while True:
+    led.plot(x, y)
+    pause(50)
+    led.unplot(x, y)
+
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+
+    if accX < -150 and x > 0:
+        x -= 1
+    if accX > 150 and x < 4:
+        x += 1
+
+    if accY < -150 and y > 0:
+        y -= 1
+    if accY > 150 and y < 4:
+        y += 1
--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-
+let accX: number;
+let accY: number;
+let x = 2
+let y = 2
+while (true) {
+    led.plot(x, y)
+    pause(50)
+    led.unplot(x, y)
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+    if (accX < -150 && x > 0) {
+        x -= 1
+    }
+    
+    if (accX > 150 && x < 4) {
+        x += 1
+    }
+    
+    if (accY < -150 && y > 0) {
+        y -= 1
+    }
+    
+    if (accY > 150 && y < 4) {
+        y += 1
+    }
+    
+}
